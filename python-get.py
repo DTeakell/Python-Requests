abb9f69fa391b8f1f0baf2f522c02b0c4e3fdf76
Dillon Teakell <teakelld@icloud.com>
2023-08-21 21:34:52 -0400

    Update

--- python-get.py
+++ python-get.py
@@ -7,12 +7,22 @@
 
 import requests
 
-def main():
+def get_api(url):
 
-    url = "https://jsonplaceholder.typicode.com/todos/1"
     response = requests.get(url)
     data = response.json()
     print(data)
 
+
+def main():
+
+    latitude = 37.3541
+    longitude = -121.9552
+
+    url = f"https://api.open-meteo.com/v1/gfs?latitude={latitude}&longitude={longitude}&current_weather"\
+    f"?latitude={latitude}"\
+    f"?longitude={longitude}"\
+    
+    get_api(url)
 
 main()
